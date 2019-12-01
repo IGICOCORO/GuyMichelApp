e14b460b2fc814da701cd219886bac0b55a3f8a8
Konstrictor <jonkur96@gmail.com>
2019-12-01 23:11:31 +0200

    added metas, unique Parole, unique Message, and ordering By date

--- api_sainte_trinite/models.py
+++ api_sainte_trinite/models.py
@@ -9,14 +9,18 @@ class Parole(models.Model):
     categorie = models.ForeignKey('Categorie', null=True, on_delete=models.SET_NULL)
     audio = models.FileField(upload_to="audio/")
     date = models.DateField(verbose_name="Date de parution", default=timezone.now)
-    slug = models.CharField(max_length=1000)
+    slug = models.CharField(max_length=1000, blank=True, null=True)
 
     def __str__(self):
     	return self.titre
 
     def save(self, *args, **kwargs):
-        if not self.slug: self.slug = str(self.categorie)+slugify(self.titre)
+        self.slug: self.slug = str(self.categorie)+slugify(self.titre)
         super(Parole, self).save(*args, **kwargs)
+
+    class Meta:
+        unique_together = ('titre', 'categorie')
+        ordering = ['-date']
 
 class Message(models.Model):
     titre = models.CharField(unique=True, max_length=100)
@@ -34,6 +38,10 @@ class Message(models.Model):
         self.slug = str(self.categorie)+slugify(self.titre)
         super(Message, self).save(*args, **kwargs)
 
+    class Meta:
+        unique_together = ('titre', 'categorie')
+        ordering = ['-date']
+
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -46,7 +54,7 @@ class Profile(models.Model):
 
 class Categorie(models.Model):
     image = models.ImageField(null=True, blank=True, upload_to="categorie/")
-    name = models.CharField(max_length=40)
+    name = models.CharField(max_length=40, unique=True)
 
     def __str__(self):
         return self.name
